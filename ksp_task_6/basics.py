3ef96b10ffa4688a94f7cd32ab0415a0a484638b
wajidhussain001 <147056326+wajidhussain001@users.noreply.github.com>
2023-10-15 16:56:03 +0530

    Add files via upload

---
+++ ksp_task_6/basics.py
@@ -0,0 +1,11 @@
+print("hello world")
+
+num1 = 10
+
+num2 = 20
+
+print(num1+num2)
+
+def addition():
+    print("hello world")
+addition()
---
+++ ksp_task_6/scripts.js
@@ -0,0 +1,38 @@
+let num1 = 12;
+let num2 = 13;
+
+console.log(num1<num2 && num2==13);
+
+console.log(!(num1==23 / num2==53));
+console.log(!(num1==12 && num2==13));
+
+function reading(){
+console.log("hello world");
+}
+reading();
+
+function playing(){
+    console.log("hello world playing");
+    }
+    playing();
+
+    function addition(digit1,digit2){
+
+        console.log(digit1 + digit2);
+        }
+        addition(22,23);
+
+        function subtraction(digit1,digit2){
+
+            console.log(digit2 - digit1);
+            }
+            subtraction(22,23);
+
+            let id ="syed wajid";
+
+            console.log(id);
+
+            console.log("hello world");
+            for(let i=0;i<10;i++){
+                console.log("hello world");
+            }
